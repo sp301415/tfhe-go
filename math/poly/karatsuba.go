7f5226096837b7de758b6a5ee271731844c92f0e
Hwang In Tak <sp301415@gmail.com>
2023-05-11 13:41:56 +0000

    fix: Smaller karatsuba buffers

--- math/poly/karatsuba.go
+++ math/poly/karatsuba.go
@@ -24,33 +24,23 @@ type karatsubaBuffer[T num.Integer] struct {
 	d2 []T
 }
 
-// karatsubaTreeIndex returns the index of the buffer value
-// based on depth and index.
-func karatsubaTreeIndex(depth, index int) int {
-	return ((num.Pow(3, depth) - 1) / 2) + index
-}
-
 // newKaratsubaBuffer allocates a slice of karatsuba buffer.
 // Each value of the buffer can be accessed with karatsubaTreeIndex.
 func newKaratsubaBuffer[T num.Integer](N int) []karatsubaBuffer[T] {
 	// The full depth of the tree = log2(N / KaratsubaThreshold)
 	fullDepth := num.Log2(N / KaratsubaRecurseThreshold)
-	// Number of nodes = 3^0 + 3^1 + ... + 3^depth = (3^(depth+1) - 1) / 2
-	buff := make([]karatsubaBuffer[T], (num.Pow(3, fullDepth+1)-1)/2)
-
+	// We only need fullDepth buffers.
+	buff := make([]karatsubaBuffer[T], fullDepth)
 	for i := 0; i < fullDepth; i++ {
-		for j := 0; j < num.Pow(3, i); j++ {
-			treeIdx := karatsubaTreeIndex(i, j)
-
-			// In depth i, karatsuba inputs have size N / 2^i
-			buff[treeIdx] = karatsubaBuffer[T]{
-				a0: make([]T, N>>(i+1)),
-				a1: make([]T, N>>(i+1)),
-				d0: make([]T, N>>i),
-				d1: make([]T, N>>i),
-				d2: make([]T, N>>i),
-			}
+		// In depth i, karatsuba inputs have size N / 2^i
+		buff[i] = karatsubaBuffer[T]{
+			a0: make([]T, N>>(i+1)),
+			a1: make([]T, N>>(i+1)),
+			d0: make([]T, N>>i),
+			d1: make([]T, N>>i),
+			d2: make([]T, N>>i),
 		}
+
 	}
 
 	return buff
@@ -80,9 +70,9 @@ func (e Evaluater[T]) mulInPlaceKaratsuba(p, q, pOut Poly[T]) {
 	q0, q1 := q.Coeffs[:N/2], q.Coeffs[N/2:]
 
 	// d0 := p0 * q0
-	e.karatsuba(p0, q0, buff.d0, 1, 0)
+	e.karatsuba(p0, q0, buff.d0, 1)
 	// d1 := p1 * q1
-	e.karatsuba(p1, q1, buff.d1, 1, 1)
+	e.karatsuba(p1, q1, buff.d1, 1)
 
 	// a0 := p0 + p1
 	// a1 := q0 + q1
@@ -92,7 +82,7 @@ func (e Evaluater[T]) mulInPlaceKaratsuba(p, q, pOut Poly[T]) {
 	}
 
 	// d2 := (p0 + p1) * (q0 + q1) = p0*q0 + p0*q1 + p1*q0 + p1*q1 = (p0*q1 + p1*q0) + d0 + d1
-	e.karatsuba(buff.a0, buff.a1, buff.d2, 1, 2)
+	e.karatsuba(buff.a0, buff.a1, buff.d2, 1)
 
 	// pOut := d0 + (d2 - d0 - d1)*X^N/2 + d1*X^N
 	for i := range pOut.Coeffs {
@@ -106,7 +96,7 @@ func (e Evaluater[T]) mulInPlaceKaratsuba(p, q, pOut Poly[T]) {
 
 // karatsuba is a recursive subroutine of karatsuba algorithm.
 // length of pOut is assumed to be twice of length of p and q.
-func (e Evaluater[T]) karatsuba(p, q, pOut []T, depth, index int) {
+func (e Evaluater[T]) karatsuba(p, q, pOut []T, depth int) {
 	N := len(p)
 
 	if N <= KaratsubaRecurseThreshold {
@@ -123,8 +113,7 @@ func (e Evaluater[T]) karatsuba(p, q, pOut []T, depth, index int) {
 		return
 	}
 
-	treeIdx := karatsubaTreeIndex(depth, index)
-	buff := e.buffer.karatsubaBuffer[treeIdx]
+	buff := e.buffer.karatsubaBuffer[depth]
 
 	// p = p0 * X^N/2 + p1
 	p0, p1 := p[:N/2], p[N/2:]
@@ -132,9 +121,9 @@ func (e Evaluater[T]) karatsuba(p, q, pOut []T, depth, index int) {
 	q0, q1 := q[:N/2], q[N/2:]
 
 	// d0 := p0 * q0
-	e.karatsuba(p0, q0, buff.d0, depth+1, 3*index+0)
+	e.karatsuba(p0, q0, buff.d0, depth+1)
 	// d1 := p1 * q1
-	e.karatsuba(p1, q1, buff.d1, depth+1, 3*index+1)
+	e.karatsuba(p1, q1, buff.d1, depth+1)
 
 	// a0 := p0 + p1
 	for i := range buff.a0 {
@@ -146,7 +135,7 @@ func (e Evaluater[T]) karatsuba(p, q, pOut []T, depth, index int) {
 	}
 
 	// d2 := (p0 + p1) * (q0 + q1) = p0*q0 + p0*q1 + p1*q0 + p1*q1 = (p0*q1 + p1*q0) + d0 + d1
-	e.karatsuba(buff.a0, buff.a1, buff.d2, depth+1, 3*index+2)
+	e.karatsuba(buff.a0, buff.a1, buff.d2, depth+1)
 
 	// pOut := d0 + (d2 - d0 - d1)*X^N/2 + d1*X^N
 	for i := range pOut {
