37cf50771854ab70d693cf84beb66cd391bdbb92
Hwang In Tak <sp301415@gmail.com>
2023-06-28 02:09:59 +0000

    fix: Pick better parameters

--- tfhe/params_list.go
+++ tfhe/params_list.go
@@ -26,14 +26,14 @@ var (
 
 	// ParamsUint3 ensures 3 bits of message precision.
 	ParamsUint3 = ParametersLiteral[uint64]{
-		LWEDimension:  742,
+		LWEDimension:  741,
 		GLWEDimension: 2,
 		PolyDegree:    1024,
 
 		LWEStdDev:  0.000007069849454709433,
 		GLWEStdDev: 0.00000000000000029403601535432533,
 
-		BlockSize: 2,
+		BlockSize: 3,
 
 		MessageModulus: 1 << 3,
 
@@ -49,14 +49,14 @@ var (
 
 	// ParamsUint4 ensures 4 bits of message precision.
 	ParamsUint4 = ParametersLiteral[uint64]{
-		LWEDimension:  742,
+		LWEDimension:  741,
 		GLWEDimension: 1,
 		PolyDegree:    2048,
 
 		LWEStdDev:  0.000007069849454709433,
 		GLWEStdDev: 0.00000000000000029403601535432533,
 
-		BlockSize: 2,
+		BlockSize: 3,
 
 		MessageModulus: 1 << 4,
 
@@ -72,14 +72,14 @@ var (
 
 	// ParamsUint5 ensures 5 bits of message precision.
 	ParamsUint5 = ParametersLiteral[uint64]{
-		LWEDimension:  806,
+		LWEDimension:  804,
 		GLWEDimension: 1,
 		PolyDegree:    4096,
 
 		LWEStdDev:  0.0000021515145918907506,
 		GLWEStdDev: 0.0000000000000000002168404344971009,
 
-		BlockSize: 2,
+		BlockSize: 4,
 
 		MessageModulus: 1 << 5,
 
@@ -95,14 +95,14 @@ var (
 
 	// ParamsUint6 ensures 5 bits of message precision.
 	ParamsUint6 = ParametersLiteral[uint64]{
-		LWEDimension:  914,
+		LWEDimension:  915,
 		GLWEDimension: 1,
 		PolyDegree:    8192,
 
 		LWEStdDev:  0.00000029804653749339636,
 		GLWEStdDev: 0.0000000000000000002168404344971009,
 
-		BlockSize: 2,
+		BlockSize: 5,
 
 		MessageModulus: 1 << 6,
 
@@ -124,7 +124,7 @@ var (
 		LWEStdDev:  0.00000022649232786295453,
 		GLWEStdDev: 0.0000000000000000002168404344971009,
 
-		BlockSize: 2,
+		BlockSize: 5,
 
 		MessageModulus: 1 << 7,
 
@@ -140,14 +140,14 @@ var (
 
 	// ParamsUint8 ensures 8 bits of message precision.
 	ParamsUint8 = ParametersLiteral[uint64]{
-		LWEDimension:  1016,
+		LWEDimension:  1014,
 		GLWEDimension: 1,
 		PolyDegree:    32768,
 
 		LWEStdDev:  0.0000000460803851108693,
 		GLWEStdDev: 0.0000000000000000002168404344971009,
 
-		BlockSize: 2,
+		BlockSize: 6,
 
 		MessageModulus: 1 << 8,
 
--- tfheb/params_list.go
+++ tfheb/params_list.go
@@ -5,14 +5,14 @@ import "github.com/sp301415/tfhe/tfhe"
 var (
 	// ParamsBoolean is a default parameter set for boolean TFHE.
 	ParamsBoolean = tfhe.ParametersLiteral[uint32]{
-		LWEDimension:  776,
+		LWEDimension:  777,
 		GLWEDimension: 3,
 		PolyDegree:    512,
 
 		LWEStdDev:  0.000003725679281679651,
 		GLWEStdDev: 0.0000000000034525330484572114,
 
-		BlockSize: 2,
+		BlockSize: 3,
 
 		MessageModulus: 1 << 2,
 
@@ -28,14 +28,14 @@ var (
 
 	// ParamsOriginalBoolean is parameters from the original C++ TFHE library.
 	ParamsOriginalBoolean = tfhe.ParametersLiteral[uint32]{
-		LWEDimension:  830,
+		LWEDimension:  828,
 		GLWEDimension: 2,
 		PolyDegree:    1024,
 
 		LWEStdDev:  0.000001412290588219445,
 		GLWEStdDev: 0.00000000000000029403601535432533,
 
-		BlockSize: 2,
+		BlockSize: 4,
 
 		MessageModulus: 1 << 2,
 
