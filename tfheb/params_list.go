3fc180baa7467c4cc02cd899d351dc101867da1e
Hwang In Tak <sp301415@gmail.com>
2023-08-09 11:30:40 +0000

    fix: Update boolean parameters

--- tfheb/params_list.go
+++ tfheb/params_list.go
@@ -5,47 +5,47 @@ import "github.com/sp301415/tfhe/tfhe"
 var (
 	// ParamsBoolean is a default parameter set for boolean TFHE.
 	ParamsBoolean = tfhe.ParametersLiteral[uint32]{
-		LWEDimension:  777,
-		GLWEDimension: 3,
+		LWEDimension:  723,
+		GLWEDimension: 2,
 		PolyDegree:    512,
 
-		LWEStdDev:  0.000003725679281679651,
-		GLWEStdDev: 0.0000000000034525330484572114,
+		LWEStdDev:  0.000013071021089943935,
+		GLWEStdDev: 0.00000004990272175010415,
 
 		BlockSize: 3,
 
 		MessageModulus: 1 << 2,
 
 		BootstrapParameters: tfhe.DecompositionParametersLiteral[uint32]{
-			Base:  1 << 18,
-			Level: 1,
+			Base:  1 << 6,
+			Level: 3,
 		},
 		KeySwitchParameters: tfhe.DecompositionParametersLiteral[uint32]{
-			Base:  1 << 4,
-			Level: 3,
+			Base:  1 << 3,
+			Level: 4,
 		},
 	}
 
 	// ParamsOriginalBoolean is parameters from the original C++ TFHE library.
 	ParamsOriginalBoolean = tfhe.ParametersLiteral[uint32]{
-		LWEDimension:  828,
-		GLWEDimension: 2,
+		LWEDimension:  630,
+		GLWEDimension: 1,
 		PolyDegree:    1024,
 
-		LWEStdDev:  0.000001412290588219445,
-		GLWEStdDev: 0.00000000000000029403601535432533,
+		LWEStdDev:  0.000030517578125,
+		GLWEStdDev: 0.0000000298023223876953125,
 
-		BlockSize: 4,
+		BlockSize: 3,
 
 		MessageModulus: 1 << 2,
 
 		BootstrapParameters: tfhe.DecompositionParametersLiteral[uint32]{
-			Base:  1 << 23,
-			Level: 1,
+			Base:  1 << 7,
+			Level: 3,
 		},
 		KeySwitchParameters: tfhe.DecompositionParametersLiteral[uint32]{
-			Base:  1 << 5,
-			Level: 3,
+			Base:  1 << 2,
+			Level: 8,
 		},
 	}
 )
