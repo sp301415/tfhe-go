ea31d8f3188e3305cc3c7b895e518529b521b1ff
Hwang In Tak <sp301415@gmail.com>
2023-12-03 01:04:25 +0900

    fix: Remove defined types from num

--- math/num/num.go
+++ math/num/num.go
@@ -8,22 +8,22 @@ import (
 
 // Unsigned represents the unsigned Integer type.
 type Unsigned interface {
-	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr
+	uint | uint8 | uint16 | uint32 | uint64 | uintptr
 }
 
 // Integer represents the Integer type.
 type Integer interface {
-	Unsigned | ~int | ~int8 | ~int16 | ~int32 | ~int64
+	Unsigned | int | int8 | int16 | int32 | int64
 }
 
 // Real represents the Integer and Float type.
 type Real interface {
-	Integer | ~float32 | ~float64
+	Integer | float32 | float64
 }
 
 // Number represents Integer, Float, and Complex type.
 type Number interface {
-	Real | ~complex64 | ~complex128
+	Real | complex64 | complex128
 }
 
 // Abs returns the absolute value of x.
