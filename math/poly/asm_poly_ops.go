41feb29e36cce4cfaf45da41624cb3042233322a
Hwang In Tak <sp301415@gmail.com>
2024-01-22 18:06:52 +0900

    fix: Fix monomialSubOneMulAssign

--- math/poly/asm_poly_ops.go
+++ math/poly/asm_poly_ops.go
@@ -13,17 +13,17 @@ func monomialSubOneMulAssign[T num.Integer](p0 Poly[T], d int, pOut Poly[T]) {
 	polyDegree := pOut.Degree()
 	if d < polyDegree {
 		for i, ii := 0, polyDegree-d; ii < polyDegree; i, ii = i+1, ii+1 {
-			pOut.Coeffs[i] += -p0.Coeffs[ii] - p0.Coeffs[i]
+			pOut.Coeffs[i] = -p0.Coeffs[ii] - p0.Coeffs[i]
 		}
 		for i, ii := d, 0; i < polyDegree; i, ii = i+1, ii+1 {
-			pOut.Coeffs[i] += p0.Coeffs[ii] - p0.Coeffs[i]
+			pOut.Coeffs[i] = p0.Coeffs[ii] - p0.Coeffs[i]
 		}
 	} else {
 		for i, ii := 0, 2*polyDegree-d; ii < polyDegree; i, ii = i+1, ii+1 {
-			pOut.Coeffs[i] += p0.Coeffs[ii] - p0.Coeffs[i]
+			pOut.Coeffs[i] = p0.Coeffs[ii] - p0.Coeffs[i]
 		}
 		for i, ii := d-polyDegree, 0; i < polyDegree; i, ii = i+1, ii+1 {
-			pOut.Coeffs[i] += -p0.Coeffs[ii] - p0.Coeffs[i]
+			pOut.Coeffs[i] = -p0.Coeffs[ii] - p0.Coeffs[i]
 		}
 	}
 }
