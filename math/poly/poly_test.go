4642a9880e7cf8eae33aad50d4930a02e14a3f88
Hwang In Tak <sp301415@gmail.com>
2024-06-12 05:01:28 +0000

    fix: Fix FFT benchmarks

--- math/poly/poly_test.go
+++ math/poly/poly_test.go
@@ -1,90 +1,76 @@
 package poly_test
 
 import (
+	"fmt"
+	"math"
+	"math/rand"
 	"testing"
 
-	"github.com/sp301415/tfhe-go/math/csprng"
 	"github.com/sp301415/tfhe-go/math/poly"
 )
 
 var (
-	N = 1 << 15
-
-	eval = poly.NewEvaluator[uint64](N)
-	fft  = poly.NewFourierEvaluator[uint64](N)
-
-	pOut  = eval.NewPoly()
-	fpOut = fft.NewFourierPoly()
-
-	sampler = csprng.NewUniformSamplerWithSeed[uint64](nil)
+	NLog = []int{9, 10, 11, 12, 13, 14, 15}
 )
 
-func BenchmarkOperations(b *testing.B) {
-	p0 := eval.NewPoly()
-	p1 := eval.NewPoly()
-	sampler.SampleSliceAssign(p0.Coeffs)
-	sampler.SampleSliceAssign(p1.Coeffs)
+func BenchmarkFourierOps(b *testing.B) {
+	for _, nLog := range NLog {
+		N := 1 << nLog
 
-	b.Run("Add", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
-			eval.AddAssign(p0, p1, pOut)
+		fft := poly.NewFourierEvaluator[uint64](N)
+
+		fp0 := fft.NewFourierPoly()
+		fp1 := fft.NewFourierPoly()
+		fpOut := fft.NewFourierPoly()
+
+		for i := 0; i < fft.Degree(); i++ {
+			fp0.Coeffs[i] = (2*rand.Float64() - 1.0) * math.Exp(63)
+			fp1.Coeffs[i] = (2*rand.Float64() - 1.0) * math.Exp(63)
 		}
-	})
 
-	b.Run("Neg", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
-			eval.NegAssign(p0, pOut)
-		}
-	})
+		b.Run(fmt.Sprintf("Add/%v", N), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				fft.AddAssign(fp0, fp1, fpOut)
+			}
+		})
 
-	b.Run("ScalarMul", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
-			eval.ScalarMulAssign(p0, 512, pOut)
-		}
-	})
+		b.Run(fmt.Sprintf("Sub/%v", N), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				fft.SubAssign(fp0, fp1, fpOut)
+			}
+		})
 
-	b.Run("MonomialMul", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
-			eval.MonomialMulAssign(p0, 32, pOut)
-		}
-	})
-
-	b.Run("Mul", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
-			eval.MulAssign(p0, p1, pOut)
-		}
-	})
-
-	fp0 := fft.ToFourierPoly(p0)
-	fp1 := fft.ToFourierPoly(p1)
-
-	b.Run("FourierMul", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
-			fft.MulAssign(fp0, fp1, fpOut)
-		}
-	})
-
-	b.Run("FourierStandardMul", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
-			fft.PolyMulAssign(fp0, p1, fpOut)
-		}
-	})
+		b.Run(fmt.Sprintf("Mul/%v", N), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				fft.MulAssign(fp0, fp1, fpOut)
+			}
+		})
+	}
 }
 
-func BenchmarkFFT(b *testing.B) {
-	p := eval.NewPoly()
-	sampler.SampleSliceAssign(p.Coeffs)
-	fp := fft.ToFourierPoly(p)
+func BenchmarkFourierTransform(b *testing.B) {
+	for _, nLog := range NLog {
+		N := 1 << nLog
 
-	b.Run("ToFourierPoly", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
-			fft.ToFourierPolyAssign(p, fpOut)
-		}
-	})
+		fft := poly.NewFourierEvaluator[uint64](N)
 
-	b.Run("ToPoly", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
-			fft.ToPolyAssignUnsafe(fp, pOut)
+		p := fft.NewPoly()
+		fp := fft.NewFourierPoly()
+
+		for i := 0; i < fft.Degree(); i++ {
+			p.Coeffs[i] = rand.Uint64()
 		}
-	})
+
+		b.Run(fmt.Sprintf("ToFourierPoly/%v", N), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				fft.ToFourierPolyAssign(p, fp)
+			}
+		})
+
+		b.Run(fmt.Sprintf("ToPoly/%v", N), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				fft.ToPolyAssignUnsafe(fp, p)
+			}
+		})
+	}
 }
